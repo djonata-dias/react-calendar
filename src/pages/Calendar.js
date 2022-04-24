82db63f4eb028189faaf4e1a8549c0426b93aa28
DJONATA DE SOUZA DIAS <djonata.dias@dotgroup.com.br>
2022-04-24 15:29:01 -0300

    page to render calendar component

---
+++ src/pages/Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calendar from '../components/Calendar';
+
+function CalendarPage(props) {
+  return (
+    <div className="container">
+      <Calendar />
+    </div>
+  );
+}
+
+export default CalendarPage;
